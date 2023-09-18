29f88ad2ae53aa6a0aee83176cdef8703b3bc6fd
dvereschagin <dvereschagin@lachestry.com>
2023-09-18 19:05:18 +0300

    Made CSS class and code style
    [#2241]

--- App/Model/Resource/EnvironmentResource.php
+++ App/Model/Resource/EnvironmentResource.php
@@ -14,12 +14,12 @@ class EnvironmentResource
         $section = '';
         foreach ($rowset as $row) {
             if (mb_substr($row, 0, 1) == '[') {
-                $section = mb_substr($row, 1, count($row) - 2);
+                $section = mb_substr($row, 1, strlen($row) - 2);
                 $parsedRowset[$section] = [];
             } else {
                 $separatorPlace = mb_stripos($row, '=');
                 $name = mb_substr($row, 0, $separatorPlace - 1);
-                $value = mb_substr($row, $separatorPlace + 3, count($row) - count($name) - 1);
+                $value = mb_substr($row, $separatorPlace + 3, strlen($row) - strlen($name) - 5);
                 $parsedRowset[$section][$name] = $value;
             }
         }
